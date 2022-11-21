03ba914e14681a7a91a69762813dcc13d845baff
Joaquin Robles <roblesjack@hotmail.com>
2022-11-21 15:40:34 -0800

    Add files via upload

--- IF_class_discussion.srcs/sources_1/new/WBStg.sv
+++ IF_class_discussion.srcs/sources_1/new/WBStg.sv
@@ -1,6 +1,11 @@
 `timescale 1ns / 1ps
 
 module WBStg(
-
+    input  logic         clk, reset,
+    input  logic         PCSrcE,
+    input  logic  [31:0] PCTargetE,
+    
+    output logic  [31:0] ALUResultW, ReadDataW,
+    output logic  [31:0] PCPlus4D   
     );
 endmodule

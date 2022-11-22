fe1d80c2dee12608329440035fb03fe8eb846f8e
Joaquin Robles <roblesjack@hotmail.com>
2022-11-22 15:36:42 -0800

    Basics

--- IF_class_discussion.srcs/sources_1/new/Execute.sv
+++ IF_class_discussion.srcs/sources_1/new/Execute.sv
@@ -10,10 +10,19 @@ module Execute(
    input logic      [1:0]   ResultSrcE,
    input logic      [2:0]   AluControlE,
    
+   output logic     [31:0]  PCTargetE,
    output logic     [31:0]  ALUResultM, WriteDataM, PCPlus4M,
    output logic     [4:0]   RDM,
    output logic             RegWriteM, MemWriteM,
    output logic     [1:0]   ResultSrcM
     );
+    
+    mux2 #(32)  pcmux(RD2E, IMMEXIE, AluSrcE, SrcBE);
+    adder       PCadder(PCE, IMMEXIE, PCTargetE);
+    
+    
+    
+    
+    
     Ex_Mem  Ex_Mem1();
 endmodule
